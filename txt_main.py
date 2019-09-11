19a5c144fc23b0983a73a7ddbedd4afdcbc53aae
Srihari231092 <s.srihari@outlook.com>
2019-09-10 21:10:10 -0400

    scraping steam reviews

--- txt_main.py
+++ txt_main.py
@@ -4,9 +4,6 @@ from bs4 import BeautifulSoup as soup
 import time
 import re, string
 
-
-
-
 # NewEgg example
 def new_egg_example():
     print(os.getcwd())
@@ -43,3 +40,35 @@ def new_egg_example():
             urlretrieve("https:"+img_src, fname+".jpg")
             ctr +=1
             time.sleep(0.5)
+
+
+my_url = "https://steamcommunity.com/app/1097840/reviews/?browsefilter=toprated&snr=1_5_100010_"
+
+# Opening a connection, grabbing the page
+client = uReq(my_url)
+
+# Save the download into an object
+page_html = client.read()
+
+# Close the client
+client.close()
+
+# Clean up the HTML code
+page_soup = soup(page_html, "html.parser")
+
+# Get the reviews
+review_elems = page_soup.findAll(name="div", attrs={"class":"apphub_CardTextContent"})
+
+for review_elem in review_elems:
+    date_posted = review_elem.findAll(name="div", attrs={"class":"date_posted"})[0].text
+    unclean_review = review_elem.text
+    review = ""
+    for ucr in unclean_review.split("\t"):
+        if (date_posted in ucr) or (len(ucr)==0):
+            continue
+        review += ucr
+        review += "\n"
+    # Sanity check - remove trailing white space
+    review = review.strip()
+    print(date_posted, "\n\t", review)
+    print(80*"-")
